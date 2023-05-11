363024015c7b8e61c79808291e4240e987050d8a
José Pablo Vázquez Partida <74749686+josepvazquezp@users.noreply.github.com>
2023-05-11 17:17:49 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const swaggerJsDoc= require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 const swaggerConf = require('./swagger.config');
 
+const socketio = require("socket.io");
+
 require('dotenv').config();
 
 const app = express();
@@ -18,6 +20,13 @@ const port = process.env.PORT || 3000;
 
 const swaggerDocs = swaggerJsDoc(swaggerConf);
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); // Reemplaza "http://localhost:4200" con la URL de tu aplicación Angular
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'); // Configura los métodos HTTP permitidos
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Configura los encabezados permitidos
+    next();
+  });
+  
 app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use('/', extraRoutes);
@@ -25,9 +34,28 @@ routes(app);
 
 mongoose.connect(mongoUrl).then(() =>{
     console.log('Se conecto correctamente a la base de datos');
-    app.listen(port, function() {
+    const server = app.listen(port, function() {
         console.log('app is running in port ' + port);
     });   
+
+    const io = socketio(server, {
+        cors: {
+            origins: "*",
+            methods: ["GET", "POST", "PUT", "DELETE"]
+        }
+    });
+
+    io.on("connection", socket => {
+        socket.on("sendMessage", (data) => {
+            socket.broadcast.emit("newMessage", {message: data});
+        });
+
+        socket.on("changeStatus", (data) => {
+            socket.broadcast.emit("receiveStatus", {status: data})
+        })
+
+    });
+
 }).catch(err => {
     console.log('No se pudo conectar a la base de datos', err);
 });
